56aad6e6686669aa3b185adcb4deb153ed9ce3ea
Josh Steele <josh@steelethis.com>
2015-09-06 01:48:30 -0400

    Added description of exercise 1 for chapter 2, and explained code.

--- Chapter2/Ex1/ex1.js
+++ Chapter2/Ex1/ex1.js
@@ -1,5 +1,18 @@
+// Looping a triangle
+// Write a loop that makes seven calls to console.log
+// to output the following triangle:
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
 var octothorpe = "";
 
+// While the length of octothorpe is less than 7 (currently 0)
+// Add another octothorpe and print the result to console
 while(octothorpe.length < 7) {
     octothorpe += "#";
     console.log(octothorpe);
