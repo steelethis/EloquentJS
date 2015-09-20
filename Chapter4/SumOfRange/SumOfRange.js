a4f7382bcff0e854455b7e3f0bdf8bfbcb36da76
Josh Steele <josh@steelethis.com>
2015-09-20 05:10:01 -0400

    Completed Chapter 4 Exercise 1 'The Sum of a Range'

--- Chapter4/SumOfRange/SumOfRange.js
+++ Chapter4/SumOfRange/SumOfRange.js
@@ -1,6 +1,34 @@
-function range(start, end) {
+function range(start, end, step) {
+    if (step === undefined) step = 1;
     var array = [];
-    for (var i = start; i <= end; i++) {
 
+    // If step is less than 0 the for loop needs to be set up so that
+    // it loops while i is greater than or equal to the end parameter.
+    if (step <= 0) {
+        for(var i = start; i >= end; i = i + step) {
+            array.push(i);
+        }
     }
+    else {
+        for (var i = start; i <= end; i = i + step) {
+            array.push(i);
+        }
+    }
+
+    return array;
 }
+
+function sum(numberArray) {
+    var sumOfNumbers = 0;
+    for (var i = 0; i < numberArray.length; i++) {
+        sumOfNumbers += numberArray[i];
+    }
+
+    return sumOfNumbers;
+}
+
+console.log(sum(range(1,10)));
+
+console.log(sum(range(1, 20, 3)));
+
+console.log(sum(range(5,2,-1)));
