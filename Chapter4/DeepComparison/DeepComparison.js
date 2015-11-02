838a6e7bdd4eb03bd6414e09336f3594c07f7f17
Josh Steele <josh@steelethis.com>
2015-11-01 23:25:11 -0500

    Completed Chapter 4 exercises

--- Chapter4/DeepComparison/DeepComparison.js
+++ Chapter4/DeepComparison/DeepComparison.js
@@ -1,3 +1,27 @@
 function deepEqual(value1, value2) {
+    if ((typeof value1 === "object" && typeof value2 === "object") &&
+        (value1 !== null && value2 !== null)) {
+        if (Object.keys(value1).length !== Object.keys(value2).length) {
+            return false;
+        }
 
+        for (var property in value1) {
+            if (!value2.hasOwnProperty(property)) {
+                return false;
+            }
+            return deepEqual(value1[property], value2[property]);
+        }
+    }
+    else {
+        return value1 === value2;
+    }
 }
+
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+//true
