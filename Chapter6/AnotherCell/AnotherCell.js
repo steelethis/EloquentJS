c48b5d55fb41a9551cc050c620d57a1574aa3306
joshua.steele <josh@steelethis.com>
2016-04-17 00:51:15 -0400

    Finished AnotherCell.js exercise.

--- Chapter6/AnotherCell/AnotherCell.js
+++ Chapter6/AnotherCell/AnotherCell.js
@@ -99,6 +99,21 @@ UnderlinedCell.prototype.draw = function(width, height) {
 };
 
 // StretchCell implementation of the interface (actual exercise from ex2 ch6)
+function StretchCell(inner, width, height) {
+    this.inner = inner;
+    this.width = width;
+    this.height = height;
+}
+StretchCell.prototype.minWidth = function() {
+    return Math.max(this.width, this.inner.minWidth());
+};
+StretchCell.prototype.minHeight = function() {
+    return Math.max(this.height, this.inner.minHeight());
+};
+StretchCell.prototype.draw = function(width, height) {
+    return this.inner.draw(width, height);
+};
+
 
 // Testing the interface.
 var rows = [];
@@ -135,4 +150,11 @@ function dataTable(data) {
 console.log("MOUNTAINS OUTPUT");
 console.log(drawTable(dataTable(MOUNTAINS)));
 
-// Testing StretchCell
+console.log("Testing StretchCell");
+var sc = new StretchCell(new TextCell("abc"), 1, 2);
+console.log(sc.minWidth());
+// → 3
+console.log(sc.minHeight());
+// → 2
+console.log(sc.draw(3, 2));
+// → ["abc", "   "]
