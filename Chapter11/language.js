11004cd765f1b0ef4712a4c6b734b0f283a6a438
Josh Steele <josh@steelethis.com>
2016-07-30 23:53:34 -0400

    Completed up to page 196 in Chapter 11.

--- Chapter11/language.js
+++ Chapter11/language.js
@@ -57,6 +57,37 @@ function parse(program) {
     return result.expr;
 }
 
+function evaluate(expr, env) {
+    switch(expr.type) {
+        case "value":
+            return expr.value;
+
+        case "word":
+            if (expr.name in env) {
+                return env[expr.name];
+            }
+            else {
+                throw new ReferenceError("Undefined variable: " + expr.name);
+            }
+            break;
+
+        case "apply":
+            if (expr.operator.type === "word" && expr.operator.name in specialForms) {
+                return specialForms[expr.operator.name](expr.args, env);
+            }
+
+            var op = evaluate(expr.operator, env);
+            if (typeof op !== "function") {
+                throw new TypeError("Applying a non-function.");
+            }
+
+            return op.apply(null, expr.args.map(function(arg) {
+                return evaluate(arg, env);
+            }));
+    }
+}
+
+var specialForms = Object.create(null);
 
 
 // testing
