8a681f291dfe1b667973ebe646d9c1263bdf1ed6
Josh Steele <josh@steelethis.com>
2016-08-01 22:53:37 -0400

    Completed up to Page 199 in chapter 11.

--- Chapter11/language.js
+++ Chapter11/language.js
@@ -89,7 +89,82 @@ function evaluate(expr, env) {
 
 var specialForms = Object.create(null);
 
+specialForms["if"] = function(args, env) {
+    if (args.length !== 3) {
+        throw new SyntaxError("Bad number of args to if");
+    }
+
+    if (evaluate(args[0], env) !== false) {
+        return evaluate(args[1], env);
+    }
+    else {
+        return evaluate(args[2], env);
+    }
+};
+
+specialForms["while"] = function(args, env) {
+    if (args.length !== 2) {
+        throw new SyntaxError("Bad number of args to while");
+    }
+
+    while (evaluate(args[0], env) !== false) {
+        evaluate(args[1], env);
+    }
+
+    // Since undefined does not exist in Egg, we return false,
+    // for lack of a meaningful result
+    return false;
+};
+
+specialForms["do"] = function (args, env) {
+    var value = false;
+    args.forEach(function(arg) {
+        value = evaluate(arg, env);
+    });
+    return value;
+};
+
+specialForms["define"] = function(args, env) {
+    if (args.length !== 2 || args[0].type !== "word") {
+        throw new SyntaxError("Bad use of define");
+    }
+
+    var value = evaluate(args[1], env);
+
+    env[args[0].name] = value;
+    return value;
+};
+
+var topEnv = Object.create(null);
+
+topEnv["true"] = true;
+topEnv["false"] = false;
+
+["+", "-", "*", "/", "==", "<", ">"].forEach(function(op) {
+    topEnv[op] = new Function("a, b", "return a " + op + " b;");
+});
+
+topEnv["print"] = function(value) {
+    console.log(value);
+    return value;
+};
+
+function run() {
+    var env = Object.create(topEnv);
+    var program = Array.prototype.slice.call(arguments, 0).join("\n");
+    return evaluate(parse(program), env);
+}
 
 // testing
 
 console.log(parse("+(a, 10)"));
+
+var prog = parse("if(true, false, true)");
+console.log(evaluate(prog, topEnv));
+
+run("do(define(total, 0),",
+    "   define(count, 1),",
+    "   while(<(count, 11),",
+    "       do(define(total, +(total, count)),",
+    "           define(count, +(count, 1)))),",
+    "   print(total))");
