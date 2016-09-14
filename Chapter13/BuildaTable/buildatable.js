b13678e97ceeb348f6a8d18ba07032e01d78f2a9
Josh Steele <josh@steelethis.com>
2016-09-13 21:51:54 -0400

    Completed 'Build a Table' exercise in Chapter 13.

--- Chapter13/BuildaTable/buildatable.js
+++ Chapter13/BuildaTable/buildatable.js
@@ -7,3 +7,44 @@ var MOUNTAINS = [
     {name: "Denali", height: 6168, country: "United States"},
     {name: "Popocatepetl", height: 5465, country: "Mexico"}
 ];
+
+function buildTable(data) {
+    var table = document.createElement("table");
+
+    for (var i = 0; i <= data.length; i++) {
+        if (i === 0) {
+            var headerRow = document.createElement("tr");
+            var keys = Object.keys(data[i]);
+            for (var j = 0; j < keys.length; j++) {
+                var header = document.createElement("th");
+                header.appendChild(document.createTextNode(keys[j]));
+                headerRow.appendChild(header);
+            }
+
+            table.appendChild(headerRow);
+        }
+
+        var row = document.createElement("tr");
+
+        var dataObject = data[i];
+        for (var key in dataObject) {
+            if (dataObject.hasOwnProperty(key)) {
+                var tableData = document.createElement("td");
+
+                if (!isNaN(dataObject[key])) {
+                    tableData.style.textAlign = "right";
+                }
+                var cellText = document.createTextNode(dataObject[key]);
+                tableData.appendChild(cellText);
+                row.appendChild(tableData);
+            }
+        }
+
+
+        table.appendChild(row);
+    }
+
+    return table;
+}
+
+document.body.appendChild(buildTable(MOUNTAINS));
