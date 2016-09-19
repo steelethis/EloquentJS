3621bcff7903e1c875a23329c6cf9019430d32f3
Josh Steele <josh@steelethis.com>
2016-09-18 22:30:04 -0400

    Modified eslintrc.json to disable anoyying rules. Finished elementsbytagname exercise in Chapter 13.

--- Chapter13/ElementsByTagName/elementsbytagname.js
+++ Chapter13/ElementsByTagName/elementsbytagname.js
@@ -1,6 +1,21 @@
 function byTagName(node, tagName) {
     var elementArray = [];
 
+    function findTag(nodeToSearch, nameOfTag) {
+        for (let i = 0; i < nodeToSearch.childNodes.length; i++) {
+            if (nodeToSearch.childNodes[i].nodeType === document.ELEMENT_NODE) {
+                if (nodeToSearch.childNodes[i].tagName.toLowerCase() === nameOfTag) {
+                    elementArray.push(nodeToSearch.childNodes[i]);
+                }
+                if (nodeToSearch.childNodes[i].childNodes.length > 0) {
+                    findTag(nodeToSearch.childNodes[i], nameOfTag);
+                }
+            }
+        }
+    }
+
+    findTag(node, tagName);
+    return elementArray;
 }
 
 console.log(byTagName(document.body, 'h1').length);
