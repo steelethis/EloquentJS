d3d965edbc2f0dffb2bc7043f8beb95eddc4f92e
Josh Steele <josh@steelethis.com>
2016-09-20 22:33:02 -0400

    Ch13 TheCatsHat: - Added code to make the initial version of this work.

---
+++ Chapter13/TheCatsHat/thecatshat.js
@@ -0,0 +1,17 @@
+var cat = document.querySelector("#cat");
+var hat = document.querySelector("#hat");
+
+var angle = 0, lastTime = null;
+
+function animate(time) {
+    if (lastTime !== null) {
+        angle += (time - lastTime) * 0.001;
+    }
+
+    lastTime = time;
+    cat.style.top = (Math.sin(angle) * 20) + "px";
+    cat.style.left = (Math.cos(angle) * 200) + "px";
+    requestAnimationFrame(animate);
+}
+
+requestAnimationFrame(animate);
