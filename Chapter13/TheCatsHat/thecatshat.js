0bf67df607323cc6f44b75d1c0585c1f649df300
Josh Steele <josh@steelethis.com>
2016-09-22 22:32:58 -0400

    Ch13 The Cat's Hat: - Finished exercise.

--- Chapter13/TheCatsHat/thecatshat.js
+++ Chapter13/TheCatsHat/thecatshat.js
@@ -3,14 +3,25 @@ var hat = document.querySelector("#hat");
 
 var angle = 0, lastTime = null;
 
+var initialCatPosition = {
+    "top": cat.offsetTop,
+    "left": cat.offsetLeft
+};
+var initialHatPosition = {
+    "top": hat.offsetTop,
+    "left": hat.offsetLeft
+};
+
 function animate(time) {
     if (lastTime !== null) {
         angle += (time - lastTime) * 0.001;
     }
 
     lastTime = time;
-    cat.style.top = (Math.sin(angle) * 20) + "px";
-    cat.style.left = (Math.cos(angle) * 200) + "px";
+    cat.style.top = (initialCatPosition.top + (Math.sin(angle) * 20)) + "px";
+    cat.style.left = (initialCatPosition.left + (Math.cos(angle) * 200)) + "px";
+    hat.style.top = (initialHatPosition.top + (Math.sin(angle) * -20)) + "px";
+    hat.style.left = (initialHatPosition.left + (Math.cos(angle) * -200)) + "px";
     requestAnimationFrame(animate);
 }
 
