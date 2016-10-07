403c7a6464ec76abe21abe008a70ebd6022d0684
Josh Steele <josh@steelethis.com>
2016-10-06 20:53:22 -0400

    Added log message that reports on mouse {x,y} coordinates.

--- Chapter14/MouseTrail/mousetrail.js
+++ Chapter14/MouseTrail/mousetrail.js
@@ -1,1 +1,5 @@
+var body = document.querySelector('body');
 
+body.addEventListener('mousemove', function(event) {
+    console.log('Mouse movement detected, X: ' + event.clientX + ' Y: ' + event.clientY);
+});
