3834a93221bea403b09f67eec796f058eb05b240
Josh Steele <josh@steelethis.com>
2016-10-10 19:43:40 -0400

    Ch14 - Set up mouse trails.

--- Chapter14/MouseTrail/mousetrail.js
+++ Chapter14/MouseTrail/mousetrail.js
@@ -1,5 +1,21 @@
 var body = document.querySelector('body');
 
+var trails = (function() {
+    var trailsArray = [];
+    for (var i = 0; i < 10; i++) {
+        var trail = document.createElement('div');
+        trail.setAttribute('class', 'trail');
+        trailsArray.push(trail);
+    }
+
+    return trailsArray;
+})();
+
+var count = 0;
+
+for (var i = 0; i < 10; i++) {
+    document.body.appendChild(trails[i]);
+}
 body.addEventListener('mousemove', function(event) {
-    console.log('Mouse movement detected, X: ' + event.clientX + ' Y: ' + event.clientY);
+
 });
