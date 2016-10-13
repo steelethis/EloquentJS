abda743a57ecef8a7c23cac58e59cb74715eb88e
Josh Steele <josh@steelethis.com>
2016-10-12 22:31:54 -0400

    Ch14 - Initial mouse trails are done.

--- Chapter14/MouseTrail/mousetrail.js
+++ Chapter14/MouseTrail/mousetrail.js
@@ -5,6 +5,7 @@ var trails = (function() {
     for (var i = 0; i < 10; i++) {
         var trail = document.createElement('div');
         trail.setAttribute('class', 'trail');
+        trail.setAttribute('id', 'trail_' + i);
         trailsArray.push(trail);
     }
 
@@ -17,5 +18,8 @@ for (var i = 0; i < 10; i++) {
     document.body.appendChild(trails[i]);
 }
 body.addEventListener('mousemove', function(event) {
-
+    var trail = document.getElementById('trail_' + (count % 10));
+    trail.style.left = event.clientX + 'px';
+    trail.style.top = event.clientY + 'px';
+    count++;
 });
