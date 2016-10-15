8b9a4b6660dc2b7b1e69b773c7e98e5ec0b4b104
Josh Steele <josh@steelethis.com>
2016-10-15 00:03:13 -0400

    Ch14 - Attractor now somewhat working for the mouse trails.

--- Chapter14/MouseTrail/mousetrail.js
+++ Chapter14/MouseTrail/mousetrail.js
@@ -18,8 +18,32 @@ for (var i = 0; i < 10; i++) {
     document.body.appendChild(trails[i]);
 }
 body.addEventListener('mousemove', function(event) {
-    var trail = document.getElementById('trail_' + (count % 10));
-    trail.style.left = event.clientX + 'px';
-    trail.style.top = event.clientY + 'px';
+    var mouseX = event.clientX;
+    var mouseY = event.clientY;
+
     count++;
+    requestAnimationFrame(attractor(mouseX, mouseY));
 });
+
+function attractor(mouseX, mouseY) {
+    var speed = 2;
+    for (var i = 0; i < trails.length; i++) {
+        var trail = document.getElementById('trail_' + i);
+        var trailX = trail.getBoundingClientRect().left;
+        var trailY = trail.getBoundingClientRect().top;
+
+        if (trailX < mouseX) {
+            trail.style.left = (trailX + speed) + 'px';
+        }
+        if (trailX > mouseX) {
+            trail.style.left = (trailX - speed) + 'px';
+        }
+        if (trailY < mouseY) {
+            trail.style.top = (trailY + speed) + 'px';
+        }
+        if (trailY > mouseY) {
+            trail.style.top = (trailY - speed) + 'px';
+        }
+    }
+
+}
