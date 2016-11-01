2e118b7ae8b02d5b737bc0b0a2edfd07556f2579
Josh Steele <josh@steelethis.com>
2016-10-31 20:42:20 -0400

    Working through Ch15

--- Chapter15/PlatformGame.js
+++ Chapter15/PlatformGame.js
@@ -29,10 +29,59 @@ function Level(plan) {
     })[0];
     this.status = this.finishDelay = null;
 }
-
 Level.prototype.isFinished = function() {
     return this.status !== null && this.finishDelay < 0;
+};
+
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
 }
+Vector.prototype.plus = function(other) {
+    return new Vector(this.x + other.x, this.y + other.y);
+};
+Vector.prototype.times = function(factor) {
+    return new Vector(this.x * factor, this.y * factor);
+};
+
+function Player(pos) {
+    this.pos = pos.plus(new Vector(0, -0.5));
+    this.size = new Vector(0.8, 1.5);
+    this.speed = new Vector(0, 0);
+}
+Player.prototype.type = 'player';
+
+function Lava(pos, ch) {
+    this.pos = pos;
+    this.size = new Vector(1, 1);
+
+    if (ch === '=') {
+        this.speed = new Vector(2, 0);
+    }
+    else if (ch === '|') {
+        this.speed = new Vector(0, 2);
+    }
+    else if (ch === 'v') {
+        this.speed = new Vector(0, 3);
+        this.repeatPos = pos;
+    }
+}
+Lava.prototype.type = 'lava';
+
+function Coin(pos) {
+    this.basePos = this.pos = pos.plus(new Vector(0.2, 0.1));
+    this.size = new Vector(0.6, 0.6);
+    this.wobble = Math.random() * Math.PI * 2;
+}
+Coin.prototype.type = 'coin';
+
+var actorChars = {
+    '@': Player,
+    'o': Coin,
+    '=': Lava,
+    '|': Lava,
+    'v': Lava
+};
 
 var simpleLevelPlan = [
     "                      ",
@@ -46,3 +95,6 @@ var simpleLevelPlan = [
     "                      "
 ];
 
+
+var simpleLevel = new Level(simpleLevelPlan);
+console.log(simpleLevel.width, "by", simpleLevel.height);
