7c1188e0351909069d1d5abdc9b6b9ec3d71f1a1
Josh Steele <josh@steelethis.com>
2016-11-01 22:13:59 -0400

    Working through Ch15

--- Chapter15/PlatformGame.js
+++ Chapter15/PlatformGame.js
@@ -75,6 +75,49 @@ function Coin(pos) {
 }
 Coin.prototype.type = 'coin';
 
+function elt(name, className) {
+    var elt = document.createElement(name);
+    if (className) {
+        elt.className = className;
+    }
+    return elt;
+}
+
+function DOMDisplay(parent, level) {
+    this.wrap = parent.appendChild(elt("div", "game"));
+    this.level = level;
+
+    this.wrap.appendChild(this.drawBackground());
+    this.actorLayer = null;
+    this.drawFrame();
+}
+var scale = 20;
+DOMDisplay.prototype.drawBackground = function() {
+    var table = elt("table", "background");
+    table.style.width = this.level.width * scale + "px";
+    this.level.grid.forEach(function(row) {
+        var rowElt = table.appendChild(elt("tr"));
+        rowElt.style.height = scale + "px";
+        row.forEach(function(type) {
+            rowElt.appendChild(elt("td", type));
+        });
+    });
+    return table;
+};
+DOMDisplay.prototype.drawActors = function() {
+    var wrap = elt("div");
+    this.level.actors.forEach(function(actor) {
+        var rect = wrap.appendChild(elt("div",
+                                        "actor " + actor.type));
+        rect.style.width = actor.size.x * scale + "px";
+        rect.style.height = actor.size.y * scale + "px";
+        rect.style.left = actor.pos.x * scale + "px";
+        rect.style.top = actor.pos.y * scale + "px";
+    });
+    return wrap;
+};
+
+
 var actorChars = {
     '@': Player,
     'o': Coin,
